0db6f4474aa04006d04e01047e5f7b39b3dac8cf
leosawka <56418748+leosawka@users.noreply.github.com>
2024-12-13 19:19:50 -0300

    SVG domain changes

--- src/components/ToogleDarkMode/ToggleDarkMode.tsx
+++ src/components/ToogleDarkMode/ToggleDarkMode.tsx
@@ -1,6 +1,5 @@
 import { useAppDispatch, useAppSelector } from "../../sotre/hooks";
 import { toggleTheme } from "../../sotre/themeSlice";
-import "../../../public/sun.svg"
 
 function ToggleDarkMode() {
   const darkMode = useAppSelector((state: { theme: { darkMode: any; }; }) => state.theme.darkMode);
@@ -16,10 +15,10 @@ function ToggleDarkMode() {
         checked={darkMode}
         readOnly
       />
-      <img src="../../../public/sun.svg"/>
+      <img src="/sun.svg"/>
       <div className="relative w-11 h-6 bg-accent-silver peer-focus:outline-none rounded-full peer dark:bg-accent-silver peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-accent-gold after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-accent-gold dark:after:bg-accent-silver after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-accent-silver dark:peer-checked:bg-accent-gold"></div>
       <span className="ms-0 text-sm font-medium text-gray-300">
-      <img src="../../../public/moon.svg"/>
+      <img src="/moon.svg"/>
       </span>
     </label>
   );
